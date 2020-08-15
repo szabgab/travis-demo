fc908e4fc1d48b9d35ee9e6c49a7c8b82adea4b2
Gabor Szabo <gabor@szabgab.com>
2020-08-15 22:49:57 +0300

    matrix test

--- test_python.py
+++ test_python.py
@@ -1,7 +1,14 @@
 import os
+import pytest
 
 print(f"DATABASE = {os.environ['DATABASE']}")
 
+@pytest.mark.postgresql
+def test_postgresql():
+    print("Testing postgresql")
+    pass
 
-def test_anything():
+@pytest.mark.mysql
+def test_mysql():
+    print("Testing mysql")
     pass
